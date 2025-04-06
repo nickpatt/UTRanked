5a81458bfd52ba81fd1247acee46e22e1a6692cf
reculas <nickhil2006@gmail.com>
2025-04-06 01:37:08 -0500

    new frontend w category

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import VotingPage from './pages/VotingPage';
-import LeaderboardPage from './pages/LeaderboardPage.tsx';
+import Home from './pages/Home';
+import RideListings from './pages/RideListings';
+import CreateRide from './pages/CreateRide';
+import Profile from './pages/Profile';
+import Auth from './pages/Auth';
 
 function App() {
   return (
-    <div className="min-h-screen bg-black">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<VotingPage />} />
-        <Route path="/leaderboard" element={<LeaderboardPage />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rides" element={<RideListings />} />
+            <Route path="/create-ride" element={<CreateRide />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/auth" element={<Auth />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
